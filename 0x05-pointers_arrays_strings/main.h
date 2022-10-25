d5340a93f81baee47054ca35f9470b6b2f69d9ac
hensburg <113614120+hensburg@users.noreply.github.com>
2022-10-24 17:32:08 -0700

    Reset 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * prototype files
+ * import from here
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+void *_strcpy(char *dest, char *scr);
+int _atol(char *s);
+
+#endif
