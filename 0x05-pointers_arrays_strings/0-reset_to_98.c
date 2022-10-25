6c270b65ab9f26129da61cf8ba6fd52712afda5f
hensburg <113614120+hensburg@users.noreply.github.com>
2022-10-24 18:16:01 -0700

    updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * reset_to_98 - take a pointer and update it to 98
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
  * import from here
  */
 
-int _putchar(char c);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int strlen(char *s);
